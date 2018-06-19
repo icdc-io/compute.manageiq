9b742511a5a47fda9471a20b7053ef11ebeedcb5
abekasau <abekasau@ibagroup.eu>
2018-06-19 14:04:55 +0300

    updated haproxy

--- app/models/haproxy_cluster.rb
+++ app/models/haproxy_cluster.rb
@@ -14,20 +14,20 @@ class HaproxyCluster < ApplicationRecord
     cluster = HaproxyCluster.new
     cluster.name = params["name"]
     cluster.hostname = params["hostname"]
-    cluster.ssh_key = params["ssh_key"]
+    cluster.api_key = params["api_key"]
     cluster.save
   end
 
   def get_service_routes(object)
     conn = connection
-    request = get_request("/api/1/service/#{object.id}")
+    request = get_request("/api/1/routes?service=#{object.id}")
     response = conn.request(request)
     HaproxyRoute.show(response.body)
   end
 
   def get_service_route(_object, id)
     conn = connection
-    request = get_request("/api/1/route/#{id}")
+    request = get_request("/api/1/routes/#{id}")
     response = conn.request(request)
     HaproxyRoute.show(response.body)
   end
@@ -35,17 +35,20 @@ class HaproxyCluster < ApplicationRecord
   def add_service_routes(object, data)
     body = generate_body(object, data)
     conn = connection
-    request = post_request("/api/1/route", body)
+    request = post_request("/api/1/routes", body)
     response = conn.request(request)
-    issue_data = {"name" => "New haproxy request for #{User.current_user.email}", "description" => "Haproxy config: #{body}", "priority" => 3}
-    Issue.new_support_issue(issue_data)
+    if response.kind_of?(Net::HTTPSuccess)
+      user = User.current_user ? User.current_user.email : "system"
+      issue_data = {"name" => "New haproxy request for #{user}", "description" => "Haproxy config: #{body}", "priority" => 3}
+      Issue.new_support_issue(issue_data)
+    end
     response
   end
 
   def edit_service_routes(object, data)
     body = generate_body(object, data)
     conn = connection
-    request = put_request("/api/1/route/#{data["route_id"]}", body)
+    request = put_request("/api/1/routes/#{data["route_id"]}", body)
     response = conn.request(request)
     issue_data = {"name" => "New haproxy request for #{User.current_user.email}", "description" => "Haproxy config: #{body}", "priority" => 3}
     Issue.new_support_issue(issue_data)
@@ -53,14 +56,72 @@ class HaproxyCluster < ApplicationRecord
   end
 
   def delete_service_routes(_object, data)
+    data = data.symbolize_keys
+
     conn = connection
-    request = get_request("/api/1/route/#{data["route_id"]}")
+    request = get_request("/api/1/routes/#{data[:route_id]}")
     response = conn.request(request)
-    issue_data = {"name" => "Haproxy delete request for #{User.current_user.email}", "description" => "Haproxy config: #{response.body}", "priority" => 3}
-    request = delete_request("/api/1/route/#{data["route_id"]}")
+    haproxy_config = response.body
+
+    request = delete_request("/api/1/routes/#{data[:route_id]}")
     response = conn.request(request)
-    Issue.new_support_issue(issue_data)
+
+    if response.kind_of?(Net::HTTPSuccess)
+      user = User.current_user ? User.current_user.email : "system"
+      issue_data = {"name" => "Haproxy delete request for #{user}", "description" => "Haproxy config: #{haproxy_config}", "priority" => 3}
+      Issue.new_support_issue(issue_data)
+    end
+
     response
+  end
+
+  def suspend_service_route(_object, id)
+    conn = connection
+    request = put_request("/api/1/routes/#{id}/suspend", {})
+    response = conn.request(request)
+    response
+  end
+
+  def get_certificates
+    conn = connection
+    request = get_request("/api/1/certs?owner=#{User.current_user.userid}")
+    conn.request(request)
+  end
+
+  def add_certificate(_object, data)
+    data[:owner] = User.current_user.userid
+
+    data["cert"] = data["cert"]["base64"]
+
+    data["key"] =
+      data["key"] ? data["key"]["base64"] : ''
+
+    data["ca"] =
+      data["ca"] ? data["ca"]["base64"] : ''
+
+    conn = connection
+    request = post_request("/api/1/certs", data)
+    response = conn.request(request)
+    response
+  end
+
+  def delete_certificate(_object, data)
+    conn = connection
+    request = delete_request("/api/1/certs/#{data["cert_id"]}")
+    response = conn.request(request)
+    response
+  end
+
+  def check_certificate(domain)
+    conn = connection
+    request = get_request("/api/1/certs?domain=#{domain}")
+    conn.request(request)
+  end
+
+  def routes
+    conn = connection
+    request = get_request("/api/1/routes")
+    conn.request(request)
   end
 
   private
@@ -73,13 +134,13 @@ class HaproxyCluster < ApplicationRecord
 
   def get_request(url)
     request = Net::HTTP::Get.new(url)
-    request.add_field("AUTH", ssh_key)
+    request.add_field("X_HAPROXYAPI_KEY", api_key)
     request
   end
 
   def post_request(url, body)
     request = Net::HTTP::Post.new(url)
-    request.add_field("AUTH", ssh_key)
+    request.add_field("X_HAPROXYAPI_KEY", api_key)
     _log.info("DBG send body #{body.to_json}")
     request.body = body.to_json
     request
@@ -87,7 +148,7 @@ class HaproxyCluster < ApplicationRecord
 
   def put_request(url, body)
     request = Net::HTTP::Put.new(url)
-    request.add_field("AUTH", ssh_key)
+    request.add_field("X_HAPROXYAPI_KEY", api_key)
     _log.info("DBG send body #{body.to_json}")
     request.body = body.to_json
     request
@@ -95,29 +156,33 @@ class HaproxyCluster < ApplicationRecord
 
   def delete_request(url)
     request = Net::HTTP::Delete.new(url)
-    request.add_field("AUTH", ssh_key)
+    request.add_field("X_HAPROXYAPI_KEY", api_key)
     request
   end
 
   def generate_body(object, data)
+    data = data.symbolize_keys
     body = {
-      "name"           => data["name"],
-      "host"           => data["dns"],
-      "proto"          => data["externalProtocol"].downcase,
+      "name"           => data[:name],
+      "host"           => data[:dns],
+      "proto"          => data[:externalProtocol].downcase,
       "service_id"     => object.id,
       "approve_status" => "approved",
+      "project"        => data[:project],
+      "purpose"        => data[:purpose],
       "backend"        => {
         "balance" => "roundrobin",
-        "proto"   => data["internalProtocol"].downcase,
+        "proto"   => data[:internalProtocol].downcase,
         "servers" => [],
       },
     }
 
     @region = object.region_number
 
-    data["vms"].each do |vm|
-      raise "Wrong or private IP address #{vm["host"]}" unless ip_allowed(vm["host"])
-      server = {"name" => vm["id"], "host" => vm["host"], "port" => data["port"]}
+    data[:vms].each do |vm|
+      vm = vm.symbolize_keys
+      raise "Wrong or private IP address #{vm[:host]}" unless ip_allowed(vm[:host])
+      server = {"name" => vm[:id], "host" => vm[:host], "port" => data[:port]}
       body["backend"]["servers"].push(server)
     end
     body
