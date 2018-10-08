2a123d5ad1aad995207fdf9ba7fe5fd42b170bd6
root <root@manageiq-idc-0.manageiq-idc.miq-dev2.svc.cluster.local>
2018-10-08 11:17:50 +0000

    save

--- app/models/mixins/account_chargeback_mixin.rb
+++ app/models/mixins/account_chargeback_mixin.rb
@@ -3,10 +3,11 @@ module AccountChargebackMixin
 
   def group_bydate_and_sum(chbs)
     grouped_chbs = chbs.group_by { |chb| chb[:start_date] }
+    #_log.info("DBG api grouped chbs #{chbs}")
     summed_costs = grouped_chbs.map do |start_date, gr_chbs|
       sum = 0
       gr_chbs.each do|chb|
-      sum += chb["cost"] 
+      sum += chb["cost"] || 0 
       end
       {"start_date"=>start_date, "cost"=>sum}
     end
