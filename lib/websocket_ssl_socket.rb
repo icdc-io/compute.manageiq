bc9374dd97606d2b4d20dc025dac9f6d1e346ace
Hanna Novikava <hnovikava@iba.by>
2018-10-25 08:37:50 +0000

    next change for console

--- lib/websocket_ssl_socket.rb
+++ lib/websocket_ssl_socket.rb
@@ -3,8 +3,8 @@ class WebsocketSSLSocket < WebsocketRight
     super(socket, model)
 
     context = OpenSSL::SSL::SSLContext.new
-    context.cert = OpenSSL::X509::Certificate.new(File.open('certs/server.cer'))
-    context.key = OpenSSL::PKey::RSA.new(File.open('certs/server.cer.key'))
+    context.cert = OpenSSL::X509::Certificate.new(File.open('certs/icdc.io.cer'))
+    context.key = OpenSSL::PKey::RSA.new(File.open('certs/icdc.io.key'))
     context.ssl_version = :SSLv23
     context.verify_depth = OpenSSL::SSL::VERIFY_NONE
     @ssl = OpenSSL::SSL::SSLSocket.new(@sock, context)
