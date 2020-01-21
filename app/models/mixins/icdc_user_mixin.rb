2a3d100e0e6d5a9b729643ed0752e455b871b1cd
Nina Uspenskaya <nuspenskaya@ibagroup.eu>
2020-01-21 12:22:45 +0300

    Ssk key

--- app/models/mixins/icdc_user_mixin.rb
+++ app/models/mixins/icdc_user_mixin.rb
@@ -4,6 +4,7 @@ module IcdcUserMixin
   # rubocop:disable Naming/AccessorMethodName, Lint/MissingCopEnableDirective
 
   included do
+    virtual_attribute :ssh_keys, :string
     virtual_column :get_user_subnets, :type => :string
     virtual_column :get_user_available_subnets, :type => :string
   end
@@ -36,5 +37,18 @@ module IcdcUserMixin
 
     available_networks = Icdc::Foreman::Client.new(miq_region.region).free_ips(all_user_networks.collect { |x| x["subnet"] }).reject { |entry| entry[:ip].nil? }.map { |entry| entry[:subnet] }
     all_user_networks.select { |entry| available_networks.include?(entry["subnet"]) }
+  end
+
+  def ssh_keys
+    {:ssh_keys => available_keys}
+  end
+
+  def available_keys
+    result = []
+    GenericObject.all.select { |go| go.generic_object_definition_name == "SshKey" }.select { |go| go.user.first }.select { |go| go.user.first.email == email }.map do |go|
+      template = { :name => go.name, :key => go["properties"]["key"], :id => go.id }
+      result.push(template)
+    end
+    result
   end
 end
