7205c4098ae310e7b4a4662f1f1897b6a617afa1
Aliaksei Hrechushkin <ahrechushkin@ibagroup.eu>
2020-05-11 15:13:24 +0300

    Update icdc_gems.rb

--- bundler.d/icdc_gems.rb
+++ bundler.d/icdc_gems.rb
@@ -1,7 +1,17 @@
-override_gem 'manageiq-schema', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-schema.git", branch: "icdc_j"
-override_gem 'manageiq-api', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-api.git", branch: "icdc_j"
-override_gem 'manageiq-automation_engine', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-automation_engine.git", branch: "icdc_j"
-override_gem 'manageiq-ui-classic', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-ui-classic.git", branch: "icdc_j"
-override_gem 'manageiq-providers-ovirt', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-providers-ovirt.git", branch: "icdc_j"
-override_gem 'manageiq-providers-power_systems', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-providers-power_systems.git", branch: "master"
-override_gem 'hmc-sdk-ruby', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/hmc-sdk-ruby.git", branch: "master"
+if ENV["RAILS_ENV"] == "production"
+	override_gem 'manageiq-schema', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-schema.git", branch: "icdc_j"
+	override_gem 'manageiq-api', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-api.git", branch: "icdc_j"
+	override_gem 'manageiq-automation_engine', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-automation_engine.git", branch: "icdc_j"
+	override_gem 'manageiq-ui-classic', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-ui-classic.git", branch: "icdc_j"
+	override_gem 'manageiq-providers-ovirt', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-providers-ovirt.git", branch: "icdc_j"
+	override_gem 'manageiq-providers-power_systems', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/manageiq-providers-power_systems.git", branch: "icdc_j"
+	override_gem 'hmc-sdk-ruby', git: "https://#{ENV['GIT_CRED']}/icdc/compute/miq/hmc-sdk-ruby.git", branch: "icdc_j"
+else
+	override_gem 'manageiq-schema', :path => File.expand_path('/opt/manageiq/manageiq-schema')
+	override_gem 'manageiq-api', :path => File.expand_path('/opt/manageiq/manageiq-api')
+	override_gem 'manageiq-automation_engine', :path => File.expand_path('/opt/manageiq/manageiq-automation_engine')
+	override_gem 'manageiq-ui-classic', :path => File.expand_path('/opt/manageiq/manageiq-ui-classic')
+	override_gem 'manageiq-providers-ovirt', :path => File.expand_path('/opt/manageiq/manageiq-providers-ovirt')
+	override_gem 'manageiq-providers-power_systems', :path => File.expand_path('/opt/manageiq/manageiq-providers-power_systems')
+	override_gem 'hmc-sdk-ruby', :path => File.expand_path('/opt/manageiq/hmc-sdk-ruby')
+end
