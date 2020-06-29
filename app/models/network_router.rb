8afe231c9d00691b3b61332a8b6a6562e2a06c94
Aliaksei Hrechushkin <ahrechushkin@ibagroup.eu>
2020-06-29 13:44:37 +0300

    Backend assign / remove network route

--- app/models/network_router.rb
+++ app/models/network_router.rb
@@ -50,6 +50,22 @@ class NetworkRouter < ApplicationRecord
     ext_management_system && ext_management_system.class::NetworkRouter
   end
 
+  def assign_route(data = nil)
+   raise ArgumentError.new("No arguments match") unless data
+   network_service = self.ext_management_system.openstack_handle.detect_network_service
+   routes = network_service.get_router(self.ems_ref).dig(:body, "router", "routes")
+   routes << data
+   network_service.update_router(self.ems_ref, {:routes => routes})
+  end
+
+  def remove_route(data = nil)
+    raise ArgumentError.new("No arguments match") unless data
+    network_service = self.ext_management_system.openstack_handle.detect_network_service
+    routes = network_service.get_router(self.ems_ref).dig(:body, "router", "routes")
+    routes.delete_if { |route| route["destination"] == data["destination"] }
+    network_service.update_router(self.ems_ref, {:routes => routes})
+  end
+
   private
 
   def extra_attributes_save(key, value)
