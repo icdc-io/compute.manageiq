aa2d9b9e0893bdbfdbf606a092741f92e1f100cc
root <root@compute-server.cua.icdc.io>
2020-11-20 12:53:57 +0200

    Move registration logic into icdc::account::user

--- lib/icdc/account.rb
+++ lib/icdc/account.rb
@@ -1,6 +1,70 @@
 module Icdc
   module Account
 
+    class User
+      def self.register(params)
+        user = ::User.new
+        user.userid = params[:user_name]
+        user.email = params[:user_name]
+        user.name = "#{params[:user_firstname]} #{params[:user_lastname]}"
+        tenants = default_tenants(user)
+        # By default we assign member role for all accounts
+        user.miq_groups = tenants.map do |tenant|
+          tenant.miq_groups.detect{ |group| group.name.end_with?(".member") }
+	      end
+	      user.save!
+
+        # Sync User objects to child regions if configured
+        tenants.each do |tenant|
+          next if tenant.regional_tenants.count <= 1 # no child regions for this tenant
+          regions = tenant.regional_tenants.collect { |d| d.region.description }
+          args = (regions + [ user.userid ]).join('_').to_s
+          %x[rake users_sync:sync_user[#{args}]]
+        end
+        user
+      end
+
+      def self.default_tenants(user)
+        [
+          Lotus.new.get_account(user),
+          Tenant.in_my_region.where(:name => "Demo").first
+        ].compact
+      end
+    end
+
+    class Lotus
+      require 'net/http'
+      require 'json'
+      require 'apipie-bindings'
+      attr_reader :logger, :config, :conn
+
+      def initialize
+        @logger = Logger.new(File.join(Rails.root, "log", "lotus.log"))
+        @config = YAML.load_file(File.join(Rails.root, "config", "lotus.yml"))
+        @conn = ApipieBindings::API.new({:uri => @config[:url], :timeout => 5})
+      end
+
+      def get_account(username)
+        user = conn.resource(:users).call(:index, :key => config[:api_key], :filter => {:mail => username}).deep_symbolize_keys
+        logger.debug("Lotus Employee object for #{username}: #{user.inspect}")
+        department_id = user[:data].first[:relationships][:department][:data][:id]
+        department = conn.resource(:departments).call(:show, :key => config[:api_key], :id => department_id).deep_symbolize_keys
+        logger.debug("Lotus Department object for #{username}: #{department.inspect}")
+        # account is an 'IBA department', so it located at second field from the top structure
+        account_id = department[:data][:attributes][:path].second
+        account = conn.resource(:departments).call(:show, :key => config[:api_key], :id => account_id).deep_symbolize_keys
+        logger.debug("Lotus Department object for account: #{account.inspect}")
+        account_name = account[:data][:attributes][:name]
+        account_tenant = Tenant.in_my_region.where(:description => account_name).first
+        logger.info("Lotus #{username} is an IBA employee. Account: #{account_tenant.inspect}")
+        account_tenant
+      rescue => e
+        logger.info("Lotus #{username} is not IBA employee. Error: #{e.message}")
+        nil
+      end
+
+    end
+
     class CRD
       attr_reader :account_name, :config, :network_service
 
@@ -23,8 +87,8 @@ module Icdc
       end
 
       def create_network(network_config)
-	      network_config.map do |type, opts|
-	        network = network_service.networks.new(:name => "#{account_name}_#{type}", :mtu => opts['mtu'])
+        network_config.map do |type, opts|
+          network = network_service.networks.new(:name => "#{account_name}_#{type}", :mtu => opts['mtu'])
           network.save
           # CloudNetwork.refresh_wait(network.id)
           # CloudNetwork.find_by(:ems_ref => network.id).add_description({'description' => "#{account_name} #{type} network"})
