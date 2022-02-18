0201df42fe4d55ddb209bf08c354404833af8fef
Aliaksei Hrechushkin <ahrechushkin@ibagroup.eu>
2022-02-18 15:43:22 +0300

    change location info on fly

--- app/models/miq_ui_worker/runner.rb
+++ app/models/miq_ui_worker/runner.rb
@@ -3,6 +3,12 @@ class MiqUiWorker::Runner < MiqWorker::Runner
 
   def prepare
     super
+    update_region_info
     MiqApache::Control.start if MiqEnvironment::Command.is_podified?
+  end
+
+  def update_region_info
+    content = ERB.new(File.new('config/locations.js.erb').read).result(binding)
+    File.open('public/ui/service/js/locations.js', 'w') { |f| f.write content }
   end
 end
