e067a536ca07b271b8d9f99d623958d4412d9e0d
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2023-11-17 17:38:28 +0300

    Add vm_id support for backup schedule

--- app/models/miq_schedule.rb
+++ app/models/miq_schedule.rb
@@ -257,10 +257,15 @@ class MiqSchedule < ApplicationRecord
   end
 
   def action_service_backup(_obj, at)
-     uri = { "namespace" => "GenericObject/Methods", "class" => "Redhat", "instance" => "create" }
-     options = {"service_id" =>"#{schedulable_id}","backup_name"=>"Scheduled backup"}
-     user = User.find_by(userid: userid)
-     AutomationRequest.create_from_ws("1.1", user, uri, options, { 'auto_approve' => true })
+    uri = { "namespace" => "GenericObject/Methods", "class" => "Redhat", "instance" => "create" }
+
+    vm_id = sched_action.dig(:options, :vm_id) # vm_id can be 'all' or id or nil
+    vm_name = vm_id == 'all' ? vm_id : Vm.find_by(id: vm_id)&.name
+
+    options = { "service_id" => "#{schedulable_id}", "backup_name" => "Scheduled backup for #{vm_name} VM",
+                "vm_id" => "#{vm_id}" }
+    user = User.find_by(userid: userid)
+    AutomationRequest.create_from_ws("1.1", user, uri, options, { 'auto_approve' => true })    
   end
 
   def action_check_compliance(obj, _at)
