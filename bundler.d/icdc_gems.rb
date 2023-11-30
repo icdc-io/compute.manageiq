3cf9e7abc708de9f5689f8f8a59db28178b9a161
ahrechushkin@ibagroup.eu <ahrechushkin@ibagroup.eu>
2023-11-30 10:54:49 +0000

    Update icdc_gems.rb

--- bundler.d/icdc_gems.rb
+++ bundler.d/icdc_gems.rb
@@ -1,15 +1,13 @@
 if ENV["RAILS_ENV"] == "production" || ENV["RAILS_ENV"] == "test"
-=begin ahrechushkin JUST FOR TESTS
   override_gem 'manageiq-schema', git: "https://#{ENV['GIT_AUTH']}@#{ENV['GIT_HOST']}/icdc/compute/miq/manageiq-schema.git", branch: "icdc_j"
   override_gem 'manageiq-api', git: "https://#{ENV['GIT_AUTH']}@#{ENV['GIT_HOST']}/icdc/compute/miq/manageiq-api.git", branch: "icdc_j"
   override_gem 'manageiq-automation_engine', git: "https://#{ENV['GIT_AUTH']}@#{ENV['GIT_HOST']}/icdc/compute/miq/manageiq-automation_engine.git", branch: "icdc_j"
   override_gem 'manageiq-ui-classic', git: "https://#{ENV['GIT_AUTH']}@#{ENV['GIT_HOST']}/icdc/compute/miq/manageiq-ui-classic.git", branch: "icdc_j"
   override_gem 'manageiq-providers-ovirt', git: "https://#{ENV['GIT_AUTH']}@#{ENV['GIT_HOST']}/icdc/compute/miq/manageiq-providers-ovirt.git", branch: "icdc_j"
-=end
 #  override_gem 'manageiq-providers-power_systems', git: "https://#{ENV['GIT_AUTH']}@#{ENV['GIT_HOST']}/icdc/compute/miq/manageiq-providers-power_systems.git", branch: "icdc_j"
 #  override_gem 'hmc-sdk-ruby', git: "https://#{ENV['GIT_AUTH']}@#{ENV['GIT_HOST']}/icdc/compute/miq/hmc-sdk-ruby.git", branch: "icdc_j"
   #override_gem 'manageiq-decorators', git: "https://#{ENV['GIT_AUTH']}@#{ENV['GIT_HOST']}/icdc/compute/miq/manageiq-decorators.git", branch: "icdc_j"
-
+=begin
 
   override_gem 'manageiq-schema', git: "https://code.zby.scdc.io/icdc/compute/miq/manageiq-schema.git", branch: "icdc_j"
   override_gem 'manageiq-api', git: "https://code.zby.scdc.io/icdc/compute/miq/manageiq-api.git", branch: "icdc_j"
@@ -17,6 +15,7 @@ if ENV["RAILS_ENV"] == "production" || ENV["RAILS_ENV"] == "test"
   override_gem 'manageiq-ui-classic', git: "https://code.zby.scdc.io/icdc/compute/miq/manageiq-ui-classic.git", branch: "icdc_j"
   override_gem 'manageiq-providers-ovirt', git: "https://code.zby.scdc.io/icdc/compute/miq/manageiq-providers-ovirt.git", branch: "icdc_j"
   override_gem 'manageiq-decorators', git: "https://code.zby.scdc.io/icdc/compute/miq/manageiq-decorators.git", branch: "icdc_j"
+=end
 elsif ENV["RAILS_ENV"] == "development"
   override_gem 'manageiq-schema', :path => File.expand_path('/opt/manageiq/manageiq-schema')
   override_gem 'manageiq-api', :path => File.expand_path('/opt/manageiq/manageiq-api')
